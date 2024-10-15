fa31576c9c85ce1cb3c15243e24ca2e0310952bb
beshoy_younan <beshoy.younan@outlook.com>
2024-10-15 16:05:02 -0300

    forms

--- src/main/java/com/beshoy/mvc/thymeleafpractice/controllers/HelloWorldController.java
+++ src/main/java/com/beshoy/mvc/thymeleafpractice/controllers/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.beshoy.mvc.thymeleafpractice.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -17,4 +20,37 @@ public class HelloWorldController {
         return "helloworld";
 }
 
+
+// need a controller mothod to read from data
+// add data to the model
+@RequestMapping("/processformv2")
+    public String letShoutDude(HttpServletRequest request, Model model){
+
+        // read request paarms from html form
+String theName =request.getParameter("studentName");
+
+    // convert the data to upper case
+theName = theName.toUpperCase();
+    // create the message
+String result = " Yo! " + theName;
+    // add message to the model
+model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+
+    @RequestMapping("/processformv3")
+    public String processFormv3(@RequestParam("studentName") String theName,
+                                Model model){
+
+        // convert the data to upper case
+        theName = theName.toUpperCase();
+        // create the message
+        String result = " Yo yooo yoooo messing you! " + theName;
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
 }
